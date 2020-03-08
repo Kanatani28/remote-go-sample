3508e9b6761d884307a2e4b4f32eb7bd9581dce6
Kanatani28 <5genbassnohito.maroro0208@gmail.com>
2020-03-08 16:34:00 +0900

    Update using docker-compose

--- src/main/server.go
+++ src/main/server.go
@@ -6,18 +6,52 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"hello"
 	"io"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func handle(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, hello.Hello())
+	// DB接続部分。接続文字列は(MySQLユーザー名:パスワード@tcp(ホスト名:ポート)/DB名)
+	db, err := sql.Open("mysql", "test:test@tcp(db:3306)/sample")
+
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	stmt, err := db.Prepare("select * from users where id < ?")
+	if err != nil {
+		panic(err)
+	}
+	defer stmt.Close()
+
+	var (
+		id   int
+		name string
+	)
+	rows, err := stmt.Query(2)
+
+	for rows.Next() {
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(id, name)
+	}
+	defer rows.Close()
+
+	if err = rows.Err(); err != nil {
+		panic(err)
+	}
+	io.WriteString(w, "Hello, "+name)
 }
 
 func main() {
-	portNumber := "9000"
+	portNumber := "1323"
 	http.HandleFunc("/", handle)
 	fmt.Println("Server listening on port ", portNumber)
 	http.ListenAndServe(":"+portNumber, nil)
